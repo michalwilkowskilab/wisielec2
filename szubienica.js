5deaf5eb7a222ee601a2ed33a491eca89d081176
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-08-23 10:49:48 +0200

    Add loop for haslo1

--- szubienica.js
+++ szubienica.js
@@ -1,8 +1,18 @@
 var haslo = "Bez pracy nie ma kołaczy";
+haslo = haslo.toUpperCase();
+
+var dlugosc = haslo.length;
+
+var haslo1 = "";
+
+for(i=0; i<dlugosc; i++){
+    if(haslo[i]==" ") haslo1 = haslo1 + " ";
+    else haslo1 = haslo1 + "-";
+}
 
 function wypisz_haslo(){
 
-    document.getElementById("plansza").innerHTML = haslo;
+    document.getElementById("plansza").innerHTML = haslo1;
 }
 
 window.onload = wypisz_haslo;
