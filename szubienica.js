b97db98d0f7b124196757c57c5c0a443c5c7aa79
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-08-23 13:15:31 +0200

    Add letters

--- szubienica.js
+++ szubienica.js
@@ -16,7 +16,46 @@ function wypisz_haslo(){
 
 window.onload = start;
 
-var litery = new Array(35)
+var litery = new Array(35);
+
+litery[0] = "A";
+litery[1] = "B";
+litery[2] = "C";
+litery[3] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ż";
+litery[34] = "Ź";
+
+
+
+
 
 function start(){
     var tresc_diva = "";
