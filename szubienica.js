97c965beeb9a7a6c2002df875d3c52537686a535
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-09-09 18:56:49 +0200

    Add ustawZnak

--- szubienica.js
+++ szubienica.js
@@ -69,9 +69,17 @@ function start(){
     wypisz_haslo();
 }
 
+String.prototype.ustawZnak = function(miejsce, znak){
+    if(miejsce > this.length - 1)return this.toString();
+    else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+}
+
 function sprawdz(nr){
 
     for(i=0; i<dlugosc; i++){
-        
+        if(haslo.charAt(i) == litery[nr]){
+            haslo1 = haslo1.ustawZnak(i, litery[nr]);
     }
+
+    wypisz_haslo();
 }
